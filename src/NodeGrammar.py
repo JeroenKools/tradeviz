421827f613b4d169229180d9c91cc50d37a341de
Jeroen Kools <koolsjeroen@gmail.com>
2023-05-16 17:49:39 -0700

    Add ai_will_propagate syntax to node grammar

--- src/NodeGrammar.py
+++ src/NodeGrammar.py
@@ -6,9 +6,12 @@ Pyparsing grammar describing the structure of the EU4 file 00_tradenodes.txt
 @author: Jeroen Kools
 """
 
-from TradeGrammar import begin, stop, eq, integer, name, quotedName, yesno, intList, floatList
+import os.path
+
 from pyparsing import ZeroOrMore, OneOrMore, Optional, Literal, Each, Group
 
+from TradeGrammar import begin, stop, eq, integer, name, quotedName, yesno, intList, floatList
+import util
 
 location = Literal("location").suppress() + eq + integer.setResultsName("location")
 color = Literal("color") + eq + intList
@@ -17,36 +20,38 @@ nameLine = (Literal("name").suppress() + eq + quotedName)
 path = (Literal("path") + eq + intList).suppress()
 control = (Literal("control") + eq + floatList).suppress()
 outgoing = Literal("outgoing") + eq + begin + \
-     nameLine + path + Optional(control) + stop
+           nameLine + path + Optional(control) + stop
 
 member = Group(Literal("members") + eq + intList)
+aiWillPropagate = Literal("ai_will_propagate_through_trade") + eq + yesno
 isEnd = Literal("end") + eq + yesno
 isInland = Literal("inland") + eq + yesno
 
-
-node = Group(name.setResultsName("name") + eq + begin + \
-    Each([location,
-          Optional(color),
-          Optional(isInland),
-          ZeroOrMore(outgoing),
-          OneOrMore(member),
-          Optional(isEnd)]) + \
-    stop)
+node = Group(name.setResultsName("name") + eq + begin +
+             Each([location,
+                   Optional(color),
+                   Optional(isInland),
+                   ZeroOrMore(outgoing),
+                   OneOrMore(member),
+                   Optional(isEnd),
+                   Optional(aiWillPropagate)
+                   ]
+                  ) +
+             stop)
 nodes = OneOrMore(node)
 
-
 if __name__ == "__main__":
-    f = open(r"C:\Program Files (x86)\Steam\steamapps\common\Europa Universalis IV\common\tradenodes\00_tradenodes.txt")
-    txt = f.read()
-    import tradeviz
-    txt = tradeviz.removeComments(txt)
-    results = nodes.parseString(txt)
-
-    nLocations = txt.count("location")
-    nFound = len(results)
+    installDir = util.getInstallDir()
+    tradeNodesTextPath = os.path.join(installDir, R"common\tradenodes\00_tradenodes.txt")
+    print("Looking for trade nodes definitions file at", repr(tradeNodesTextPath))
+    with open(tradeNodesTextPath) as f:
+        txt = f.read()
+        txt = util.removeComments(txt)
+        results = nodes.parseString(txt)
+        nLocations = txt.count("location")
+        nFound = len(results)
 
     print(f"Found {nFound} out of {nLocations} tradenodes")
 
-#     for r in results:
-#         print r["name"], r["location"]
-
+    for r in results:
+        print(r["name"], r["location"])
